f0ba907423f0710c5c6394b340ee2f04dd3c5f1d
417-72KI <417.72ki@gmail.com>
2023-01-01 03:29:07 +0900

    remove unnecessary dependencies (added on template)

--- twitter4k-core/build.gradle.kts
+++ twitter4k-core/build.gradle.kts
@@ -14,21 +14,12 @@ repositories {
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.twitter4j:twitter4j-core:4.0.7")
 
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:30.1.1-jre")
-
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
 }
 
 tasks {
