5f84392496dc3599c8183d623e2e2fb9810ebaad
417-72KI <417.72ki@gmail.com>
2023-01-05 01:27:12 +0900

    use `CheckstyleFormat` plugin

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -1,8 +1,10 @@
 @file:Repository("https://repo.maven.apache.org/maven2/")
 @file:DependsOn("io.github.ackeecz:danger-kotlin-junit:0.1.0")
+@file:DependsOn("io.github.417-72ki:danger-kotlin-checkstyle_format:0.0.2")
 @file:OptIn(ExperimentalPathApi::class)
 
 import io.github.ackeecz.danger.junit.JUnitPlugin
+import jp.room417.danger_kotlin_checkstyle_format.CheckstyleFormat
 import systems.danger.kotlin.*
 import java.io.File
 import java.io.IOException
@@ -13,6 +15,7 @@ import kotlin.io.path.*
 import kotlin.streams.toList
 
 register plugin JUnitPlugin
+register plugin CheckstyleFormat
 
 danger(args) {
     val path = Path(System.getProperty("user.dir"))
@@ -32,7 +35,12 @@ danger(args) {
         }
     }
     path.forEachDirectoryEntryRecursive(glob = "**/build/reports/ktlint/**/ktlint*.xml") {
-        warn("TODO: parse `$it`")
+        CheckstyleFormat.basePath = if (System.getenv("CI") == "true") {
+            Path("/home/runner/work/Twitter4Kt/Twitter4Kt")
+        } else {
+            Path(System.getenv("WORKING_DIR") ?: System.getProperty("user.dir"))
+        }
+        CheckstyleFormat.report(it)
     }
 
     path.forEachDirectoryEntryRecursive(glob = "**/build/test-results/*/*.xml") {
