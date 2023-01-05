809b1adcd9310938f48a64d0f92454eb623f6571
417-72KI <417.72ki@gmail.com>
2023-01-05 23:33:39 +0900

    move implements of `Twitter` wrapper

--- twitter4kt-core/src/main/kotlin/jp/room417/twitter4kt/Twitter.kt
+++ twitter4kt-core/src/main/kotlin/jp/room417/twitter4kt/Twitter.kt
@@ -2,15 +2,11 @@ package jp.room417.twitter4kt
 
 import jp.room417.twitter4kt.api.TimelinesResources
 import jp.room417.twitter4kt.api.TweetsResources
-import jp.room417.twitter4kt.api.internal.TimelinesResourcesImpl
-import jp.room417.twitter4kt.api.internal.TweetsResourcesImpl
 import jp.room417.twitter4kt.auth.OAuthSupport
+import jp.room417.twitter4kt.internal.TwitterImpl
 import jp.room417.twitter4kt.util.letWith
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import twitter4j.TwitterFactory
 import twitter4j.auth.AccessToken
-import twitter4j.auth.RequestToken
 
 /** A wrapper of [twitter4j.Twitter] */
 interface Twitter : OAuthSupport {
@@ -25,14 +21,11 @@ interface Twitter : OAuthSupport {
 
     @Suppress("unused", "MemberVisibilityCanBePrivate")
     class Builder(
-        private var consumerKey: String?,
-        private var consumerSecret: String?,
-        private var accessToken: AccessToken?
+        private val factory: TwitterFactory = TwitterFactory(),
+        private var consumerKey: String? = null,
+        private var consumerSecret: String? = null,
+        private var accessToken: AccessToken? = null
     ) {
-        constructor() : this(null, null, null)
-
-        private val factory = TwitterFactory()
-
         fun setOAuthConsumer(
             consumerKey: String,
             consumerSecret: String
@@ -58,87 +51,5 @@ interface Twitter : OAuthSupport {
                 setOAuthAccessToken(it)
             }
         }
-    }
-}
-
-// Implement
-internal class TwitterImpl(private val twitter: twitter4j.Twitter) : Twitter {
-    override val origin = twitter
-
-    override fun timelines() = TimelinesResourcesImpl(twitter)
-    override fun tweets() = TweetsResourcesImpl(twitter)
-
-    // OAuthSupport
-    override fun setOAuthConsumer(
-        consumerKey: String,
-        consumerSecret: String
-    ) = twitter.setOAuthConsumer(
-        consumerKey,
-        consumerSecret
-    )
-
-    override suspend fun getOAuthRequestToken(): RequestToken = withContext(Dispatchers.IO) {
-        twitter.oAuthRequestToken
-    }
-
-    override suspend fun getOAuthRequestToken(callbackURL: String): RequestToken =
-        withContext(Dispatchers.IO) {
-            twitter.getOAuthRequestToken(callbackURL)
-        }
-
-    override suspend fun getOAuthRequestToken(
-        callbackURL: String,
-        xAuthAccessType: String
-    ): RequestToken = withContext(Dispatchers.IO) {
-        twitter.getOAuthRequestToken(
-            callbackURL,
-            xAuthAccessType
-        )
-    }
-
-    override suspend fun getOAuthRequestToken(
-        callbackURL: String,
-        xAuthAccessType: String,
-        xAuthMode: String
-    ): RequestToken = withContext(Dispatchers.IO) {
-        twitter.getOAuthRequestToken(
-            callbackURL,
-            xAuthAccessType,
-            xAuthMode
-        )
-    }
-
-    override suspend fun getOAuthAccessToken(): AccessToken = withContext(Dispatchers.IO) { twitter.oAuthAccessToken }
-
-    override suspend fun getOAuthAccessToken(oauthVerifier: String): AccessToken = withContext(Dispatchers.IO) {
-        twitter.getOAuthAccessToken(oauthVerifier)
-    }
-
-    override suspend fun getOAuthAccessToken(requestToken: RequestToken): AccessToken = withContext(Dispatchers.IO) {
-        twitter.getOAuthAccessToken(requestToken)
-    }
-
-    override suspend fun getOAuthAccessToken(
-        requestToken: RequestToken,
-        oauthVerifier: String
-    ): AccessToken = withContext(Dispatchers.IO) {
-        twitter.getOAuthAccessToken(
-            requestToken,
-            oauthVerifier
-        )
-    }
-
-    override suspend fun getOAuthAccessToken(
-        screenName: String,
-        password: String
-    ): AccessToken = withContext(Dispatchers.IO) {
-        twitter.getOAuthAccessToken(
-            screenName,
-            password
-        )
-    }
-
-    override fun setOAuthAccessToken(accessToken: AccessToken) {
-        twitter.oAuthAccessToken = accessToken
     }
 }
--- twitter4kt-core/src/test/kotlin/jp/room417/twitter4kt/TwitterBuilderTest.kt
+++ twitter4kt-core/src/test/kotlin/jp/room417/twitter4kt/TwitterBuilderTest.kt
@@ -1,14 +1,19 @@
 package jp.room417.twitter4kt
 
+import twitter4j.TwitterFactory
 import kotlin.test.Test
-import kotlin.test.assertTrue
+import kotlin.test.assertEquals
 
 class TwitterBuilderTest {
     @Test
     fun build() {
-        val twitterInstance = Twitter.Builder()
+        val twitterInstance = Twitter.Builder(FakeTwitterFactory())
             .setOAuthConsumer("foo", "bar")
             .build()
-        assertTrue(twitterInstance is TwitterImpl)
+        val clazz = Class.forName("jp.room417.twitter4kt.internal.TwitterImpl").kotlin
+        assertEquals(twitterInstance.javaClass.name, clazz.qualifiedName)
     }
+
+    // TODO implement fake
+    class FakeTwitterFactory : TwitterFactory()
 }
