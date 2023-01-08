f0505638c4fd72f35659ef89c6f7bd5d605b75e1
417-72KI <417.72ki@gmail.com>
2023-01-09 02:23:31 +0900

    fix `v1` instance

--- twitter4kt-core/src/main/kotlin/jp/room417/twitter4kt/v1/TwitterV1.kt
+++ twitter4kt-core/src/main/kotlin/jp/room417/twitter4kt/v1/TwitterV1.kt
@@ -2,51 +2,44 @@ package jp.room417.twitter4kt.v1
 
 import jp.room417.twitter4kt.v1.api.*
 
+/** A wrapper of [twitter4j.v1.TwitterV1] */
 interface TwitterV1 {
-    /** A wrapper of [twitter4j.Twitter.timelines] */
+    /** A wrapper of [twitter4j.v1.TwitterV1.timelines] */
     fun timelines(): TimelinesResources
 
-    /** A wrapper of [twitter4j.Twitter.tweets] */
+    /** A wrapper of [twitter4j.v1.TwitterV1.tweets] */
     fun tweets(): TweetsResources
 
-    /** A wrapper of [twitter4j.Twitter.search] */
+    /** A wrapper of [twitter4j.v1.TwitterV1.search] */
     fun search(): SearchResource
 
-    /** A wrapper of [twitter4j.Twitter.directMessages] */
+    /** A wrapper of [twitter4j.v1.TwitterV1.directMessages] */
     fun directMessages(): DirectMessagesResources
 
-    /** A wrapper of [twitter4j.Twitter.friendsFollowers] */
+    /** A wrapper of [twitter4j.v1.TwitterV1.friendsFollowers] */
     fun friendsFollowers(): FriendsFollowersResources
 
-    /** A wrapper of [twitter4j.Twitter.users] */
+    /** A wrapper of [twitter4j.v1.TwitterV1.users] */
     fun users(): UsersResources
 
-    /** A wrapper of [twitter4j.Twitter.suggestedUsers] */
-    @Suppress("DEPRECATION")
-    @Deprecated(
-        message = "Deprecated from Twitter and will be removed on Twitter4j 4.1.0.\nSee https://twittercommunity.com/t/upcoming-changes-to-user-object-and-get-users-suggestions-endpoints/124732",
-        level = DeprecationLevel.WARNING
-    )
-    fun suggestedUsers(): SuggestedUsersResources
-
-    /** A wrapper of [twitter4j.Twitter.favorites] */
+    /** A wrapper of [twitter4j.v1.TwitterV1.favorites] */
     fun favorites(): FavoritesResources
 
-    /** A wrapper of [twitter4j.Twitter.list] */
+    /** A wrapper of [twitter4j.v1.TwitterV1.list] */
     fun list(): ListsResources
 
-    /** A wrapper of [twitter4j.Twitter.savedSearches] */
+    /** A wrapper of [twitter4j.v1.TwitterV1.savedSearches] */
     fun savedSearches(): SavedSearchesResources
 
-    /** A wrapper of [twitter4j.Twitter.placesGeo] */
+    /** A wrapper of [twitter4j.v1.TwitterV1.placesGeo] */
     fun placesGeo(): PlacesGeoResources
 
-    /** A wrapper of [twitter4j.Twitter.trends] */
+    /** A wrapper of [twitter4j.v1.TwitterV1.trends] */
     fun trends(): TrendsResources
 
-    /** A wrapper of [twitter4j.Twitter.spamReporting] */
+    /** A wrapper of [twitter4j.v1.TwitterV1.spamReporting] */
     fun spamReporting(): SpamReportingResource
 
-    /** A wrapper of [twitter4j.Twitter.help] */
+    /** A wrapper of [twitter4j.v1.TwitterV1.help] */
     fun help(): HelpResources
 }
